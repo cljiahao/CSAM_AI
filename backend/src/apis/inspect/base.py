93425775d1f4a9e1c1e66989523cd9c7a13e5ebc
CLJiaHao <clarence.lee@murata.com>
2023-10-12 17:40:26 +0800

    added no_of_batches to return to frontend

--- backend/src/apis/inspect/base.py
+++ backend/src/apis/inspect/base.py
@@ -33,15 +33,15 @@ def inspect(image, lot_no, chip_type, db):
     no_of_chips
         Number of chips found
     """
-    no_of_chips, NG, save_dir, pred_dir = check_dir(image, lot_no, db)
+    no_of_chips, no_of_batches, NG, save_dir, pred_dir = check_dir(image, lot_no, db)
     border_img, img_shape = create_border_img(image, save_dir)
-    if any(NG.values()): return NG, save_dir, img_shape, no_of_chips                # If exists, return to quicken retrieval
+    if any(NG.values()): return NG, save_dir, img_shape, no_of_batches, no_of_chips                # If exists, return to quicken retrieval
 
     batch_data = mask(border_img, img_shape)
     no_of_chips, pred_dict, ng_dict = chips(border_img, batch_data)
     pred_dict_res = prediction(chip_type, pred_dict)
 
-    ng_dict.update(pred_dict_res)                                                   # Update ng_dict with predicted ng
+    ng_dict.update(pred_dict_res)                                                                   # Update ng_dict with predicted ng
 
     no_of_batches = len(batch_data)
     NG = {}
@@ -50,7 +50,7 @@ def inspect(image, lot_no, chip_type, db):
     for key,value in ng_dict.items():
 
         ng_img=cv2.cvtColor(value,cv2.COLOR_RGB2BGR)
-        cv2.imwrite(os.path.join(pred_dir,key),ng_img)                              # Writing NG images into directory
+        cv2.imwrite(os.path.join(pred_dir,key),ng_img)                                              # Writing NG images into directory
 
         if int(key.split("_")[1]) != 0 : NG["Batch " + key.split("_")[1]].append(key)
         elif "Stray" in NG.keys(): NG["Stray"].append(key)
