088cd7bb2b47955a9668e8a392258439c44da4b5
CLJiaHao <clarence.lee@murata.com>
2023-10-12 17:41:38 +0800

    Added caching for pred and real images

--- backend/src/apis/inspect/components/initialize.py
+++ backend/src/apis/inspect/components/initialize.py
@@ -37,21 +37,33 @@ def check_dir(image, lot_no, db):
     lot_dir = os.path.join(backend_dir,'images',datetime.now().strftime("%b%y"),lot_no)
     save_dir = os.path.join(lot_dir,plate_no)
     pred_dir = os.path.join(save_dir,"pred")
+    real_dir = os.path.join(save_dir,"real")
 
     NG = {}
     no_of_chips = 0
-    if lot_no.lower()[:4] == "test" and os.path.isdir(lot_dir): rmtree(lot_dir)
-    if os.path.isdir(pred_dir) and any(os.scandir(pred_dir)): 
-        ratio = get_ratio(lot_no=lot_no, plate_no=plate_no, db=db)
+    if lot_no.lower()[:4] == "test" and os.path.isdir(lot_dir): rmtree(lot_dir)                 # Testing directory to be remove every testing
+    if os.path.isdir(pred_dir) and any(os.scandir(pred_dir)):
+        ratio = get_ratio(lot_no=lot_no, plate_no=plate_no, db=db)                              # Pull data from database for caching
         no_of_chips = ratio.no_of_chips
-        for i in range(len(ratio.no_of_batches)): NG[f"Batch {i+1}"] = []
-        for prevChips in os.listdir(pred_dir):
-            if prevChips.split(".")[-1] == ".png":
-                if int(prevChips.split("_")[1]) != 0: NG["Batch " + prevChips.split("_")[1]].append(prevChips)
-                elif "Stray" in NG.keys(): NG["Stray"].append(prevChips)
-                else: NG["Stray"] = []
+        no_of_batches = ratio.no_of_batches
+        for i in range(len(no_of_batches)): NG[f"Batch {i+1}"] = []
+        caching(pred_dir,NG)
+        if os.path.isdir(real_dir) and any(os.scandir(real_dir)): caching(real_dir,NG,True)
+
     else: 
         if os.path.isdir(save_dir): rmtree(save_dir) 
         os.makedirs(pred_dir)
 
-    return no_of_chips, NG, save_dir, pred_dir
+    return no_of_chips, no_of_batches, NG, save_dir, pred_dir
+
+
+def caching(directory,chips,ng=False):
+
+    for file in os.listdir(directory):
+        if file.split(".")[-1] == ".png":
+            if ng: file = "1"+file[1:]
+            if int(file.split("_")[1]) != 0: chips[f"Batch {file.split('_')[1]}"].append(file)
+            elif "Stray" in chips.keys(): chips["Stray"].append(file)
+            else: chips["Stray"] = []
+
+    return chips
