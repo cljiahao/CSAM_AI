7f189a7bdc1da152111255530588a4d886d38b7d
CLJiaHao <clarence.lee@murata.com>
2023-10-13 15:02:36 +0800

    Fixed datatypes and added arguments

--- backend/src/schemas/ng.py
+++ backend/src/schemas/ng.py
@@ -4,8 +4,8 @@ class ShowNG(BaseModel):
     plate_no: str
     ng_chips: dict
     img_shape: list
-    no_of_batches: str
-    no_of_chips: str
-    ng_count: str
+    no_of_batches: int
+    no_of_chips: int
+    ng_count: int
     directory: str
     chip_type: str
